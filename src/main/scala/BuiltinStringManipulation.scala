693dcc57000b06c1e5104336175caf4646282b2a
Edward A Maxedon, Sr <edadma@gmail.com>
2018-12-25 00:14:01 -0500

    strings

--- src/main/scala/BuiltinStringManipulation.scala
+++ src/main/scala/BuiltinStringManipulation.scala
@@ -22,6 +22,28 @@ object BuiltinStringManipulation {
       case x => sys.error( s"atom_string: expected atom: $x" )
     }
 
+  def string_chars( vm: VM, string: Any, chars: Any ) =
+    string match {
+      case _: vm.Variable =>
+        chars match {
+          case _: vm.Variable => sys.error( "string_chars: string must be given" )
+          case s: Structure =>
+            list2array( s ) match {
+              case Some( a ) =>
+                val charatoms =
+                  a map {
+                    case Symbol( c ) if c.length == 1 => c.head
+                    case _ => sys.error( s"string_chars: expected list of characters: $a" )
+                  }
+                vm.unify( new String(charatoms), string )
+              case None => sys.error( s"string_chars: expected list of character codes: $s" )
+            }
+          case x => sys.error( s"string_chars: expected list of character codes: $x" )
+        }
+      case s: String => vm.unify( array2list(s.toArray map (c => Symbol(c.toString))), chars )
+      case x => sys.error( s"string_chars: expected string: $x" )
+    }
+
   def string_codes( vm: VM, string: Any, codes: Any ) =
     string match {
       case _: vm.Variable =>
@@ -35,13 +57,27 @@ object BuiltinStringManipulation {
                     case n: Int if n.isValidChar => n.toChar
                     case _ => sys.error( s"string_codes: expected list of character codes: $a" )
                   }
-                vm.unify( new String(charcodes), codes )
+                vm.unify( new String(charcodes), string )
               case None => sys.error( s"string_codes: expected list of character codes: $s" )
             }
           case x => sys.error( s"string_codes: expected list of character codes: $x" )
         }
-      case s: String => vm.unify( array2list(s.toArray), codes )
+      case s: String => vm.unify( array2list(s.toArray map (_.toInt)), codes )
       case x => sys.error( s"string_codes: expected string: $x" )
+    }
+
+  def string_upper( vm: VM, string: Any, upper: Any ) =
+    string match {
+      case _: vm.Variable => sys.error( "string_upper: string must be given" )
+      case s: String => vm.unify( s.toUpperCase, upper )
+      case x => sys.error( s"string_upper: expected string: $x" )
+    }
+
+  def string_lower( vm: VM, string: Any, lower: Any ) =
+    string match {
+      case _: vm.Variable => sys.error( "string_lower: string must be given" )
+      case s: String => vm.unify( s.toLowerCase, lower )
+      case x => sys.error( s"string_lower: expected string: $x" )
     }
 
 }
