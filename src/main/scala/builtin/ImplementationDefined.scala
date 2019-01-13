9d4d8ace1a9084ae0ee6a84aa21020e685b9a850
Edward A Maxedon, Sr <edadma@gmail.com>
2019-01-13 14:50:28 -0500

    cont coding flags support

--- src/main/scala/builtin/ImplementationDefined.scala
+++ src/main/scala/builtin/ImplementationDefined.scala
@@ -5,21 +5,39 @@ import scala.collection.mutable
 
 object ImplementationDefined {
 
+  class Flag( val default: Any ) {
+    val changeable = false
+
+    val possible: Any => Boolean = Set[Any]()
+
+    def value = default
+
+    def value_=( newvalue: Any ): Unit = sys.error( "unchangeable flag" )
+  }
+
+  class ChangeableFlagSet( default: Any, override val possible: Set[Any], action: => Unit = {} ) extends Flag( default ) {
+    var v: Any = default
+
+    override def value = v
+
+    override def value_=( newvalue: Any ) = v = newvalue
+  }
+
   val flags =
-    mutable.HashMap[Symbol, Any] (
-      'bounded -> true,
-      'integer_rounding_function -> 'toward_zero,
-      'debug -> 'off,
-      'max_arity -> 255
+    mutable.HashMap[Symbol, Flag] (
+      'bounded -> new Flag( true ),
+      'integer_rounding_function -> new Flag( 'toward_zero ),
+      'debug -> new ChangeableFlagSet( 'off, Set[Any]('off, 'on) ),
+      'max_arity -> new Flag( 255 )
     )
   val changeable = Set( 'debug )
 
   def set_prolog_flag( vm: VM, flag: Any, value: Any ) =
     (flag, value) match {
       case (_: vm.Variable, _: vm.Variable) => sys.error( "set_prolog_flag/2: flag and value must be given" )
-      case (f: Symbol, _) if flags contains f =>
-
-      case _ => sys.error( "set_prolog_flag/2: expected valid flag and value" )
+      case (f: Symbol, _) if flags.contains(f) && flags(f).changeable =>
+        flags(f) = value
+      case _ => sys.error( "set_prolog_flag/2: expected valid changeable flag and value" )
     }
 
 }
