667401acb703346111c256a70f47035831827994
Edward A Maxedon, Sr <edadma@gmail.com>
2019-01-19 09:35:52 -0500

    operator table

--- src/main/scala/builtin/Operators.scala
+++ src/main/scala/builtin/Operators.scala
@@ -16,13 +16,45 @@ object Operators {
       Operator( 1200, 'fx, '?- ),
       Operator( 1050,	'xfy, '-> ),
       Operator( 1000,	'xfy, Symbol(",") ),
-      Operator( 900, 'fy, Symbol("\\+") )
+      Operator( 900, 'fy, Symbol("\\+") ),
+      Operator( 700, 'xfx, Symbol("\\=") ),
+      Operator( 700, 'xfx, '= ),
+      Operator( 700, 'xfx, '== ),
+      Operator( 700, 'xfx, Symbol("\\==") ),
+      Operator( 700, 'xfx, '@< ),
+      Operator( 700, 'xfx, '@=< ),
+      Operator( 700, 'xfx, '@> ),
+      Operator( 700, 'xfx, '@>= ),
+      Operator( 700, 'xfx, Symbol("=..") ),
+      Operator( 700, 'xfx, 'is ),
+      Operator( 700, 'xfx, '=:= ),
+      Operator( 700, 'xfx, '=\= ),
+      Operator( 700, 'xfx, '< ),
+      Operator( 700, 'xfx, '=< ),
+      Operator( 700, 'xfx, '> ),
+      Operator( 700, 'xfx, '>= ),
+      Operator( 500, 'yfx, '+ ),
+      Operator( 500, 'yfx, '- ),
+      Operator( 500, 'yfx, '/\ ),
+      Operator( 500, 'yfx, Symbol("\\/") ),
+      Operator( 400, 'yfx, '* ),
+      Operator( 400, 'yfx, '/ ),
+      Operator( 400, 'yfx, '// ),
+      Operator( 400, 'yfx, 'rem ),
+      Operator( 400, 'yfx, 'mod ),
+      Operator( 400, 'yfx, '<< ),
+      Operator( 400, 'yfx, '>> ),
+      Operator( 200, 'xfx, '** ),
+      Operator( 200, 'xfy, '^ ),
+      Operator( 200, 'fy, '- ),
+      Operator( 200, 'fy, Symbol("\\") )
     )
 
   def op( vm: VM, pos: IndexedSeq[Reader], priority: Any, specifier: Any, operator: Any ) =
     (priority, specifier, operator) match {
       case (p: Int, s: Symbol, _) =>
         true
+      case _ => sys.error( "op/3: expected integer, atom, atom" )
     }
 
 }
